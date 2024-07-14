d41d12030d3b4f0bd46f69135da82409afc98f1a
Enerzh AbilliKA <15370030205@163.com>
2024-07-14 11:49:25 +0800

    1.0.1 更新，较为稳定

--- find_grnLaser.py
+++ find_grnLaser.py
@@ -10,8 +10,8 @@ tilt_servo = Servo(2)
 
 grn_threshold = (91, 100, -36, 1, -3, 30)
 
-pan_pid = PID(p=0.07, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
-tilt_pid = PID(p=0.05, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
+pan_pid = PID(p=0.037, d=0.015, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
+tilt_pid = PID(p=0.035, d=0.013, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
 # pan_pid = PID(p=0.1, i=0, imax=90)#在线调试使用这个PID
 # tilt_pid = PID(p=0.1, i=0, imax=90)#在线调试使用这个PID
 
@@ -35,6 +35,17 @@ def find_max(blobs):
             max_size = blob[2] * blob[3]
     return max_blob
 
+def limmitAngle(panAngle, tiltAngle):
+    if panAngle > 15:
+        panAngle = 15
+    if panAngle < -15:
+        panAngle = -15
+    if tiltAngle > 15:
+        tiltAngle = 15
+    if tiltAngle < -15:
+        tiltAngle = -15
+    return panAngle, tiltAngle
+
 while True:
     clock.tick() # Track elapsed milliseconds between snapshots().
     img = sensor.snapshot() # Take a picture and return the image.
@@ -46,7 +57,7 @@ while True:
             cx = int(blob[0] + blob[2] / 2)
             cy = int(blob[1] + blob[3] / 2)
             pan_error = cx - img.width() / 2
-            tilt_error = cy - img.height() / 2
+            tilt_error = cy - img.height() / 2 + 5
 
             print("pan_error: ", pan_error)
 
@@ -60,5 +71,7 @@ while True:
             panAngle  = int(pan_servo.angle() - pan_output)
             tiltAngle = int(tilt_servo.angle() + tilt_output)
 
-            pan_servo.angle(panAngle)
-            tilt_servo.angle(tiltAngle)
+            # 限幅
+            x_angle, y_angle = limmitAngle(panAngle, tiltAngle)
+            pan_servo.angle(x_angle)
+            tilt_servo.angle(y_angle)
